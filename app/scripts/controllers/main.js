7c8f80ccd1fa368f319282edd89a99b4b50a2ddf
Jason Taylor <jason@redant.com.au>
2014-08-10 20:17:00 +1000

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoAngularApp
+ */
+angular.module('yoAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
